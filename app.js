046796bacaee20c9558700ad156a9695b5100ed0
Rajeev jha <rajeevjha4141@gmail.com>
2024-08-03 00:33:36 +0530

    test code

---
+++ app.js
@@ -0,0 +1,9 @@
+// Declaring variables
+let name = "Rajeev";      // String
+const age = 30;           // Number
+let isDeveloper = true;   // Boolean
+
+// Outputting variables to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Developer:", isDeveloper);
